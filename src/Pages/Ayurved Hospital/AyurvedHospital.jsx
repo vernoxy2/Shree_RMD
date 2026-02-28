817743027d53c9d1de5e1fee7370d5d9e31e3558
vernoxy <vernoxy3@gmail.com>
2026-02-28 17:30:15 +0530

    path

--- src/Pages/Ayurved Hospital/AyurvedHospital.jsx
+++ src/Pages/Ayurved Hospital/AyurvedHospital.jsx
@@ -7,7 +7,7 @@ import Campus from "./Components/Campus";
 import Services from "./Components/Services";
 import Infrastructure from "./Components/Infrastructure";
 import Diagnostics from "./Components/Diagnostics";
-import Snapshot from "./Components/snapshot";
+// import Snapshot from "./Components/snapshot";
 
 const AyurvedHospital = () => {
   return (
